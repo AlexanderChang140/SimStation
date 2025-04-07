a720d53c9852bc9e2c4e7e652cffd73e448c2747
Alex <alexanderchang140@gmail.com>
2025-04-07 12:54:41 -0700

    refactor: use Son Ha's code

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,102 +1,112 @@
 package mvc;
 
-import tools.*;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.beans.*;
+import java.util.*;
+import javax.swing.*;
 
-public abstract class AppPanel extends JPanel implements Subscriber, ActionListener {
-    public static final int FRAME_WIDTH = 500;
-    public static final int FRAME_HEIGHT = 300;
-
-    protected final AppFactory factory;
-    protected final JFrame frame;
-    protected final JPanel controlPanel;
-    protected final View view;
+public class AppPanel extends JPanel implements PropertyChangeListener, ActionListener {
+    private static final long serialVersionUID = 1L;
     protected Model model;
+    protected AppFactory factory;
+    protected Set<View> views;
+    private JFrame frame;
+    public static int FRAME_WIDTH = 600;
+    public static int FRAME_HEIGHT = 350;
 
     public AppPanel(AppFactory factory) {
+        super();
         this.factory = factory;
         model = factory.makeModel();
-        view = factory.makeView(model);
-        controlPanel = new JPanel();
-        controlPanel.setBackground(Color.LIGHT_GRAY);
-        controlPanel.setLayout(new GridLayout(1, 2));
+        views = new HashSet<View>();
+        if (model != null) model.addPropertyChangeListener(this);
 
-        this.setLayout(new GridLayout(1, 2));
-        this.add(controlPanel);
-        this.add(view);
-
-        frame = new SafeFrame();
+        frame = new JFrame();
         Container cp = frame.getContentPane();
         cp.add(this);
         frame.setJMenuBar(createMenuBar());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(factory.getTitle());
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    }
+
+    public void addView(View view) {
+        views.add(view);
+    }
+
+    @Override
+    public Component add(Component c) {
+        if (c instanceof View) addView((View) c);
+        return super.add(c);
     }
 
     public void display() {
         frame.setVisible(true);
     }
 
-    @Override
-    public void update() {
-        controlPanel.revalidate();
-        controlPanel.repaint();
+    //
+    public void propertyChange(PropertyChangeEvent evt) {
+        /* override in extensions if needed */
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    // called by file/open and file/new
+    public void setModel(Model newModel) {
+        this.model.removePropertyChangeListener(this);
+        this.model = newModel;
+        this.model.initSupport();
+        this.model.addPropertyChangeListener(this);
+        for (View view : views) view.setModel(this.model);
+        //alternatively: this.model.copy(model);
     }
 
     protected JMenuBar createMenuBar() {
         JMenuBar result = new JMenuBar();
-        JMenu fileMenu = Utilities.makeMenu("File", new String[]{"New", "Save", "Save As", "Open", "Quit"}, this);
+        // add file, edit, and help menus
+        JMenu fileMenu =
+                Utilities.makeMenu("File", new String[]{"New", "Save", "SaveAs", "Open", "Quit"}, this);
         result.add(fileMenu);
-        JMenu editMenu = Utilities.makeMenu("Edit", factory.getEditCommands(), this);
+
+        JMenu editMenu =
+                Utilities.makeMenu("Edit", factory.getEditCommands(), this);
         result.add(editMenu);
-        JMenu helpMenu = Utilities.makeMenu("Help", new String[]{"About", "Help"}, this);
+
+        JMenu helpMenu =
+                Utilities.makeMenu("Help", new String[]{"About", "Help"}, this);
         result.add(helpMenu);
+
         return result;
     }
 
-    public void actionPerformed(ActionEvent e) {
-        String cmmd = e.getActionCommand();
-        try {
-            switch (cmmd) {
-                case "Save" -> Utilities.save(model, false);
-                case "Save As" -> Utilities.save(model, true);
-                case "Open" -> {
-                    Model newModel = Utilities.open(model);
-                    if (newModel != null) {
-                        setModel(newModel);
-                    }
-                }
-                case "New" -> {
-                    Utilities.saveChanges(model);
-                    setModel(factory.makeModel());
-                }
-                case "Quit" -> {
-                    Utilities.saveChanges(model);
-                    System.exit(0);
-                }
+    public void actionPerformed(ActionEvent ae) {
+        String cmmd = ae.getActionCommand();
 
-                case "About" -> Utilities.inform(factory.getAbout());
-                case "Help" -> Utilities.inform(factory.getHelp());
-
-                default -> {
-                    factory.makeEditCommand(cmmd, model).execute();
-                    view.update();
-                }
-            }
-
-        } catch (Exception ex) {
-            Utilities.error(ex);
+        if (cmmd == "Save") {
+            Utilities.save(model, false);
+        } else if (cmmd == "SaveAs") {
+            Utilities.save(model, true);
+        } else if (cmmd == "Open") {
+            Model newModel = Utilities.open(model);
+            if (newModel != null) setModel(newModel);
+        } else if (cmmd == "New") {
+            Utilities.saveChanges(model);
+            setModel(factory.makeModel());
+            // needed cuz setModel sets to true:
+            model.setUnsavedChanges(false);
+        } else if (cmmd == "Quit") {
+            Utilities.saveChanges(model);
+            System.exit(1);
+        } else if (cmmd == "About") {
+            Utilities.inform(factory.about());
+        } else if (cmmd == "Help") {
+            Utilities.inform(factory.getHelp());
+        } else {
+            Command command = factory.makeEditCommand(model, cmmd);
+            CommandProcessor.execute(command);
         }
-    }
-
-    private void setModel(Model newModel) {
-        this.model.unsubscribe(this);
-        this.model = newModel;
-        this.model.subscribe(this);
-        view.setModel(this.model);
-        model.notifySubscribers();
     }
 }
---
+++ src/sim_station/SimStationView.java
@@ -0,0 +1,39 @@
+package sim_station;
+
+import mvc.*;
+
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+public class SimStationView extends View {
+    public static int BOX_X_CORNER = 5;
+    public static int BOX_Y_CORNER = 5;
+    public static int LINE_THICKNESS = 3;
+    public SimStationView(Model model) {
+        super(model);
+        setSize(Simulation.WORLD_SIZE,Simulation.WORLD_SIZE);
+
+    }
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+
+        Graphics2D g2 = (Graphics2D) gc;
+        Color oldColor = gc.getColor();
+
+        Simulation sim = (Simulation)model;
+        ArrayList<Agent> agents = sim.getAgents();
+        g2.setColor(Color.RED);
+
+        for (Agent a : agents) {
+            g2.fillRect(a.getX(), a.getY(), Agent.AGENT_SIZE, Agent.AGENT_SIZE);
+        }
+
+        g2.setColor(Color.BLACK);
+
+        g2.setStroke(new BasicStroke(2));
+        g2.draw(new Rectangle2D.Double(BOX_X_CORNER, BOX_Y_CORNER, Simulation.WORLD_SIZE + Agent.AGENT_SIZE, Simulation.WORLD_SIZE + Agent.AGENT_SIZE));
+        g2.setColor(oldColor);
+    }
+}
