260a448d17a45b97110043aa1c4bc22384727f3f
Alex <alexanderchang140@gmail.com>
2025-04-07 16:43:07 -0700

    refactor: clean up code fix: fix typo in stats style: auto-format

--- src/plague/PlagueSimulation.java
+++ src/plague/PlagueSimulation.java
@@ -1,35 +1,32 @@
 package plague;
 
-import sim_station.*;
+import sim_station.Simulation;
 
 public class PlagueSimulation extends Simulation {
     public static int VIRULENCE = 50; //Percent of infection
     public static int RESISTANCE = 2; //Percent of resisting infection
     public static int POP_SIZE = 50;
 
-    public PlagueSimulation (String name){
+    public PlagueSimulation(String name) {
         super(name);
     }
 
-    public void populate(){
-        for (int i = 0; i < POP_SIZE; i++){
+    public void populate() {
+        for (int i = 0; i < POP_SIZE; i++) {
             agents.add(new Plague(this));
         }
     }
 
-    public String[] getStats(){
-        double count = 0;
-        for (Agent a: agents){
-            Plague p = (Plague)a;
-            if(p.isInfected()){
-                count++;
-            }
-        }
+    @Override
+    public String[] getStats() {
+        double count = agents.stream()
+                .filter(agent -> ((Plague) agent).isInfected())
+                .count();
 
         String[] stats = new String[3];
         stats[0] = "# agents = " + agents.size();
         stats[1] = "Clock = " + clock;
-        stats[2] = "% if infected: " + Math.round(count/POP_SIZE * 100) + "%";
+        stats[2] = "% infected: " + Math.round(count / POP_SIZE * 100) + "%";
         return stats;
     }
 }
