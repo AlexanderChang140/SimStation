6eb846e08b96d922111ab2422ddbde5ecc5516fa
Alex <alexanderchang140@gmail.com>
2025-04-14 00:53:11 -0700

    feat: add alive stat for default implementation refactor: use Observer agent instead of timer refactor: remove unused field name, and deprecate respective constructor refactor: make for each loops more concise style: whitespace changes

--- src/sim_station/Simulation.java
+++ src/sim_station/Simulation.java
@@ -3,79 +3,88 @@ package sim_station;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import mvc.*;
 import sim_station.agent.Agent;
 import sim_station.agent.AgentState;
+import sim_station.agent.ObserverAgent;
 import tools.Utilities;
 
 public class Simulation extends Model {
     private static final long serialVersionUID = 1L;
     public static Integer WORLD_SIZE = 250; // height & width of the world (& view)
+    private final ObserverAgent observer = new ObserverAgent(this);
     protected ArrayList<Agent> agents;
-    private Timer timer;
+    protected int alive;
     protected int clock;
-    private String name;
 
-
+    @Deprecated
     public Simulation(String name) {
-        this.name = name;
-        timer = new Timer();
         clock = 0;
-        agents = new ArrayList<Agent>();
+        agents = new ArrayList<>();
+    }
+
+    public Simulation() {
+        clock = 0;
+        agents = new ArrayList<>();
     }
 
     public synchronized void start() {
         agents.clear();
         populate();
-        restartTimer();
-        for(Agent a : agents)
-            a.start();
+        agents.forEach(Agent::start);
+        observer.start();
         changed();
+        clock = 0;
     }
 
     public synchronized void pause() {
-        if(agents.get(0).getState() == AgentState.STOPPED) {
+        if (agents.get(0).getState() == AgentState.STOPPED) {
             Utilities.error("Model is stopped!");
             return;
         }
-        stopTimer();
-        for(Agent a : agents)
-            a.pause();
+        agents.forEach(Agent::pause);
+        observer.pause();
         changed();
     }
 
     public synchronized void resume() {
-        if(agents.get(0).getState() == AgentState.STOPPED) {
+        if (agents.get(0).getState() == AgentState.STOPPED) {
             Utilities.error("Model is stopped!");
             return;
         }
-        startTimer();
-        for(Agent a : agents)
-            a.resume();
+        agents.forEach(Agent::resume);
+        observer.resume();
         changed();
     }
 
     public synchronized void stop() {
-        stopTimer();
-        for(Agent a : agents) {
-            a.stop();
-        }
+        agents.forEach(Agent::stop);
+        observer.stop();
         changed();
     }
+
     public String[] getStats() {
-        String[] stats = new String[2];
+        String[] stats = new String[3];
         stats[0] = "# agents = " + agents.size();
         stats[1] = "clock = " + clock;
+        stats[2] = "# alive " + alive;
         return stats;
+    }
+
+    public void updateStatistics() {
+        alive = (int) agents.stream().filter(n -> n.getState() != AgentState.STOPPED).count();
+        clock++;
     }
 
     public synchronized Agent getNeighbor(Agent thisAgent) {
         Agent nextAgent = null;
-        int thisX = thisAgent.getX(); int thisY = thisAgent.getY();
+        int thisX = thisAgent.getX();
+        int thisY = thisAgent.getY();
 
-        for(int i = 0; i < agents.size(); i++) {
+        for (int i = 0; i < agents.size(); i++) {
             Agent a = agents.get(i);
-            if(i == 0) {
+            if (i == 0) {
                 nextAgent = a;
                 continue;
             }
@@ -88,7 +97,7 @@ public class Simulation extends Model {
             double diffNearY = Math.abs(nextAgent.getY() - thisY);
             double distNearThis = Math.sqrt((diffNearX * diffNearX) + (diffNearY * diffNearY));
 
-            if(distAtThis < distNearThis) {
+            if (distAtThis < distNearThis) {
                 nextAgent = a;
             }
         }
@@ -100,15 +109,15 @@ public class Simulation extends Model {
         boolean found = false;
         int i = Utilities.rng.nextInt(agents.size());
         int start = i;
-        while(!found) {
+        while (!found) {
             Agent candidate = agents.get(i);
 
-            if(candidate != asker && asker.getDistance(candidate) < radius) {
+            if (candidate != asker && asker.getDistance(candidate) < radius) {
                 neighbor = agents.get(i);
                 found = true;
             } else {
-                i = (i+1) % agents.size();
-                if(i == start)
+                i = (i + 1) % agents.size();
+                if (i == start)
                     break;
             }
         }
@@ -122,27 +131,5 @@ public class Simulation extends Model {
 
     public ArrayList<Agent> getAgents() {
         return agents;
-    }
-
-    private void startTimer() {
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
-    }
-
-    private void stopTimer() {
-        timer.cancel();
-        timer.purge();
-    }
-
-    private void restartTimer() {
-        timer.cancel();
-        timer.purge();
-        clock = 0;
-        startTimer();
-    }
-    private class ClockUpdater extends TimerTask {
-        public void run() {
-            clock++;
-        }
     }
 }
--- src/sim_station/agent/ObserverAgent.java
+++ src/sim_station/agent/ObserverAgent.java
@@ -1,4 +1,14 @@
 package sim_station.agent;
 
-public class ObserverAgent {
+import sim_station.Simulation;
+
+public class ObserverAgent extends Agent {
+    public ObserverAgent(Simulation sim) {
+        super(sim);
+    }
+
+    @Override
+    public void update() {
+        world.updateStatistics();
+    }
 }
